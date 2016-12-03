02d35103e9e0ecb405396ee6e2f6c144e1d00423
chenag <23110134@qq.com>
2016-12-03 19:36:55 +0800

    m

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 /**
  * Created by chenanguo on 2016/10/14.
  */
-console.log("console.log输出的内容");
-document.write("document.write输出的内容");
+console.log("console.log输出的内容6666666");
+document.write("document.write输出的内容666");
